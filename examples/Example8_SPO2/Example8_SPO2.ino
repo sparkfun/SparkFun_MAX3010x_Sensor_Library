86cd3959d9fcf66f0a90535b59c316f68a42f74d
Nathan Seidle <nathan@sparkfun.com>
2017-01-27 09:47:50 -0700

    Modifying Ex8 to work with SAMD21

--- examples/Example8_SPO2/Example8_SPO2.ino
+++ examples/Example8_SPO2/Example8_SPO2.ino
@@ -34,10 +34,15 @@ MAX30105 particleSensor;
 
 #define MAX_BRIGHTNESS 255
 
+#if defined(ARDUINO_AVR_UNO)
 //Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
 //To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
 uint16_t irBuffer[100]; //infrared LED sensor data
 uint16_t redBuffer[100];  //red LED sensor data
+#else
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+#endif
 
 int32_t bufferLength; //data length
 int32_t spo2; //SPO2 value
