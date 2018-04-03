ffa421553aa93b8418b5c65896ff0560d0ac008b
Nathan Seidle <nathan@sparkfun.com>
2018-04-03 08:58:37 -0600

    Fix issue 14 - change sampleRate to int in example

--- examples/Example4_HeartBeat_Plotter/Example4_HeartBeat_Plotter.ino
+++ examples/Example4_HeartBeat_Plotter/Example4_HeartBeat_Plotter.ino
@@ -51,7 +51,7 @@ void setup()
   byte ledBrightness = 0x1F; //Options: 0=Off to 255=50mA
   byte sampleAverage = 8; //Options: 1, 2, 4, 8, 16, 32
   byte ledMode = 3; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
   int pulseWidth = 411; //Options: 69, 118, 215, 411
   int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
 
