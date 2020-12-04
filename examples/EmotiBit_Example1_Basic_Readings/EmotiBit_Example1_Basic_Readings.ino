5d6ab5f04e3f5bcbadb56c42410948fa554b137b
Nitin Nair <31810812+nitin710@users.noreply.github.com>
2020-12-04 16:31:45 -0500

    Basic Example (#2)
    * modified basic example to print data which can be seen in arduino plotter.
    
    * bumped lib version to 1.1.3
    
    * changes made as requested in Review.
    
    * Added readme as per review
    
    * Update ReadMe.md
    
    * minor print addition

--- examples/EmotiBit_Example1_Basic_Readings/EmotiBit_Example1_Basic_Readings.ino
+++ examples/EmotiBit_Example1_Basic_Readings/EmotiBit_Example1_Basic_Readings.ino
@@ -19,6 +19,7 @@ MAX30105 particleSensor;
 void setup()
 {
   debug.begin(9600);
+  while(!Serial);
   debug.println("EmotiBit MAX30101 Basic Readings Example");
   pinMode(6, OUTPUT);
   digitalWrite(6, LOW);
@@ -26,25 +27,31 @@ void setup()
   pinPeripheral(11, PIO_SERCOM);
   pinPeripheral(13, PIO_SERCOM);
   myWire.setClock(100000);
+  Serial.println("Trying to connect to the sensor on I2C line");
   // Initialize sensor
   if (particleSensor.begin(myWire) == false)
   {
+    debug.println("FAILURE");
     debug.println("MAX30105 was not found. Please check wiring/power. ");
     while (1);
   }
-
+  else
+  {
+    debug.println("SUCCESS!");
+    //debug.println("open the Arduino Serial Plotter to view data plotted on screen");
+  }
   particleSensor.setup(); //Configure sensor. Use 6.4mA for LED drive
 }
 
 void loop()
 {
-  debug.print(" R[");
-  debug.print(particleSensor.getRed());
-  debug.print("] IR[");
+  //debug.print(" R[");
+  debug.print(particleSensor.getRed()); Serial.print("\t");
+  //debug.print("] IR[");
   debug.print(particleSensor.getIR());
-  debug.print("] G[");
-  debug.print(particleSensor.getGreen());
-  debug.print("]");
+  //debug.print("] G[");
+  //debug.print(particleSensor.getGreen());
+  //debug.print("]");
 
   debug.println();
 }
---
+++ examples/EmotiBit_Example1_Basic_Readings/ReadMe.md
@@ -0,0 +1,11 @@
+# EmotiBit MAX30101 Basic Example
+
+## Setup
+- Connect the emotibit to the Feather.
+- Plug in the feather to the computer and upload this example.
+
+## Run the Example
+- After the code has been uploaded, reset the feather and open the Serial Plotter in Arduino.
+- If the sensor is connected, you sould see the Serial Plotter plot values of the IR and RED leds.
+- Place your finger on the sensor. Let the plot settle and you should see the plot display your heartbeat.
+- ![](./assets/Plotter.png)
