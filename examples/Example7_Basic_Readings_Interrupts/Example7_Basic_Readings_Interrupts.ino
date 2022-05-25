e4438ef955700cecda65f5f501d861edc76fb0ac
Nathan Seidle <nathan@sparkfun.com>
2022-05-24 20:24:24 -0600

    Fix for #45

--- examples/Example7_Basic_Readings_Interrupts/Example7_Basic_Readings_Interrupts.ino
+++ examples/Example7_Basic_Readings_Interrupts/Example7_Basic_Readings_Interrupts.ino
@@ -47,7 +47,7 @@ void setup()
   byte ledBrightness = 0x7F; //Options: 0=Off to 255=50mA
   byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32
   byte ledMode = 3; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  byte sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
   int pulseWidth = 69; //Options: 69, 118, 215, 411
   int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
 
